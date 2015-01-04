3dd75d806796e2f115fc4ce56e233070b7a765cd
vishalgarg42 <vishalgarg1181014@gmail.com>
2015-01-04 13:33:14 +0530

    Dynamic work Four Charts

--- src/main/webapp/cloud-js/charts.js
+++ src/main/webapp/cloud-js/charts.js
@@ -319,6 +319,114 @@ app.directive('chartyy', function() {
 
 
 
+app.directive('chartfour', function() {
+    return {
+        restrict: 'E',
+        template: "<div style='float:left; margin-top:20px;margin-left:20px;'>hello</div>",
+        scope: {
+            chartValue: "=value",
+            chartObjfour: "=?"
+        },
+        transclude: true,
+        replace: true,
+        link: function($scope, $element, $attrs) {
+            //Update when charts data changes
+        	$attrs.$observe("chartValue", function(value) {
+       
+            	var activate=0;
+            	console.log(value)
+            	var query = "select bytesReceived from AggregateMetrics where time > now() - 3000h limit 10";
+            var s=	allok(query);
+            	console.log(s);
+            
+                // use default values if nothing is specified in the given settings
+            	$scope.chartData={};
+                $scope.chartData.chart={};
+                $scope.chartData.title={};
+                $scope.chartData.xAxis={};
+                $scope.chartData.yAxis={};
+                $scope.chartData.tooltip={};
+                $scope.chartData.legend={};
+                $scope.chartData.series={};
+               
+                
+                $scope.chartData.chart.renderTo = $scope.chartData.chart.renderTo || $element[0];
+                if ($attrs.type)
+                    $scope.chartData.chart.type = $scope.chartData.chart.type || $attrs.type;
+                if ($attrs.height)
+                    $scope.chartData.chart.height = $scope.chartData.chart.height || $attrs.height;
+                if ($attrs.width)
+                    $scope.chartData.chart.width = $scope.chartData.chart.width || $attrs.width;
+                
+                $scope.chartData.title.text='Performance Heath';
+             
+                $scope.chartData.xAxis.categories=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                                                   'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+                
+                $scope.chartData.yAxis.title={};
+                $scope.chartData.yAxis.text='Temperature (C)';
+                $scope.chartData.tooltip.valueSuffix='C';
+                $scope.chartData.legend.layout='vertical';
+                $scope.chartData.legend.align='right';
+                $scope.chartData.legend.verticalAlign='middle';
+                $scope.chartData.legend.borderWidth=0;
+             
+                $scope.chartData.series=s;
+                console.log( $scope.chartData.series.length);
+                if($scope.chartData.series.length!=0)
+                	{
+                $scope.chartObjfour = new Highcharts.Chart($scope.chartData);
+                 console.log( $scope.chartObjfour);
+                 activate=1;
+                	 }
+//                }
+            	
+                counter=0;
+                setInterval(function(){
+//                	console.log("innn");
+                	var s=	allok(query);
+                  	if(activate==0)
+                  		{
+                  	    $scope.chartData.series=s;
+                  	   $scope.chartObjfour = new Highcharts.Chart($scope.chartData);
+                       activate=1;
+                  		}
+                  	else
+                  		{
+                  	//                 		 var xseries=$scope.chartObj.xAxis.categories;
+                 	//	console.log(xseries);
+                 		 	                      var yseries =$scope.chartObjfour.series;
+//	                      console.log($scope.chartData.series);
+						  
+                         timeSeries=parseEpochTime(s[2].data);
+   				         console.log($scope.chartData.series);
+					      //logic to update xaxis timeseries data on chart every minute considering interval is 5 sec (5*12)
+						  counter++;
+//                         if(counter%12==0)
+//							xseries.push(timeSeries[0]);
+//						  else
+//						xseries.push("");
+//						
+//						  //append series data with data points for respective series
+						  for ( i=0; i<s.length; i++) {
+//						
+                               datapoints =  s[i].data[0];
+//
+//								//shift y series if dataponits are more than a value (20)
+//                           	shift = yseries[i].data.length > 20;
+//
+
+                               yseries[i].addPoint(datapoints, true, true);
+                   	  }	
+                  		}
+                	},1000);
+            });
+        }
+    };
+
+});
+
+
 
 function parseEpochTime(epoch)
 {
